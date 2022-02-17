808b10cf2b15c2ddf43d462db31b25bc518c3324
yigit tuncel <yigittuncel35@gmail.com>
2022-02-17 12:56:04 +0100

    Lists and Bookmarks Controller created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Movie.destroy_all
+p "Destroying all movies..."
+require "json"
+require "open-uri"
+
+url = "http://tmdb.lewagon.com/movie/top_rated"
+user_serialized = URI.open(url).read
+json = JSON.parse(user_serialized)
+results = json["results"]
+
+base_url = "https://image.tmdb.org/t/p/w500"
+
+results.each do |result|
+  Movie.create!(
+    title: result["title"],
+    overview: result["overview"],
+    poster_url: "#{base_url}#{result["poster_path"]}",
+    rating: result["vote_average"]
+  )
+  p "Movie #{result["title"]} is created"
+end
