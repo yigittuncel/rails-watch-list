69adbd0bde3931b03fc539dbab0a05ec239bf1bc
yigit tuncel <yigittuncel35@gmail.com>
2022-02-20 21:26:48 +0100

    code refactored

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,6 +13,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:comment, :rating)
+    params.require(:review).permit(:comment, :rating, :list_id)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :list
   validates :comment, presence: true
   validates :rating, presence: true
+  validates_inclusion_of :rating, :in => 1..5
 end
